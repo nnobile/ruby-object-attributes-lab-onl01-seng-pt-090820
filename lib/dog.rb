01e109a0d8add5704baad59fb92cf657edf6fc35
Nick Nobile <nickcnobile@gmail.com>
2020-09-25 00:41:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,15 @@
 class Dog
- 
+
   def name=(dogs_name)
     @this_dogs_name = dogs_name
   end
- 
+
   def name
     @this_dogs_name
   end
 end
- 
+
 fido = Dog.new
 fido.name = "Fido"
- 
+
 puts fido.name
