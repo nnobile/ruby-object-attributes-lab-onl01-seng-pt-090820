64813dd2b5abd5d97b67f4c22fc28854c5101a20
Nick Nobile <nickcnobile@gmail.com>
2020-09-25 00:35:09 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+  def name(name)
+    @name = "fido"
+  end
+
+  def breed(breed)
+    @breed = breed
+  end
+end
+  
+  fido = Dog.new
+  snoopy = Dog.new
+
+
+class Person
+  def name(name)
+    @name = "beyonce"
+  end
+
+  def job(job)
+    @job = job
+  end
+
+beyonce = Person.new
+
+  
